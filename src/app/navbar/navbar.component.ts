d75f3948df68af3dee77d842c76b0eb7ed1b5e2b
Bijay Pandey <bijaypandey630@gmail.com>
2023-09-28 20:50:04 +0530

    designed the ui for mobile devices

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  
+  showMenuIcon:boolean = true;
+  
+  ngOnInit() {
+    document.querySelector("nav .nav-links")?.addEventListener("click", () => {
+      this.closeMenu();
+    });
+  }
+
+  showMenu(){
+    const menuIcon = <HTMLDivElement>document.querySelector("nav .nav-links");
+    if(menuIcon){
+      menuIcon.style.left = "0";
+      this.showMenuIcon = false;
+    }
+  }
+
+  closeMenu(){
+    const menuIcon = <HTMLDivElement>document.querySelector("nav .nav-links");
+    if(menuIcon){
+      menuIcon.style.left = "-100%";
+      this.showMenuIcon = true;
+    }
+  }
 }
